1b81fd348efd79b66848cfbfdbba8fa461d9eb32
rohith.vallabhaneni <rohith.vallabhaneni@ggktech.com>
2017-03-20 10:21:39 +0530

    Testing Jenkins

--- TestJenkins/Program.cs
+++ TestJenkins/Program.cs
@@ -11,6 +11,7 @@ namespace TestJenkins
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Jenkins!!");
+            Console.WriteLine("Testing jenkins");
         }
     }
 }
