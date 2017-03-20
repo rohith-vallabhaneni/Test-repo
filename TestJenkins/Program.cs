66c69a9fc22ce68ebe9f1c90aeb1cc782fd86dba
rohith.vallabhaneni <rohith.vallabhaneni@ggktech.com>
2017-03-20 10:27:00 +0530

    Testing jenkins

---
+++ TestJenkins/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestJenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Jenkins!!");
+            Console.WriteLine("Testing jenkins");
+        }
+    }
+}
